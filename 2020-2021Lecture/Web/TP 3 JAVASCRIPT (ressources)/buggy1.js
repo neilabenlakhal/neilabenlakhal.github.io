2df373ac82f815372d293947dc2faf4d981005a1
neilabenlakhal <neila.benlakhal@gmail.com>
2022-02-16 17:29:43 +0100

    lecture folder web

---
+++ 2020-2021Lecture/Web/TP 3 JAVASCRIPT (ressources)/buggy1.js
@@ -0,0 +1,3 @@
+function gogogo() {
+    alert("Yay, it works!");
+}
---
+++ 2020-2021Lecture/Web/TP 3 JAVASCRIPT (ressources)/buggy4.js
@@ -0,0 +1,11 @@
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
+
+function gogogo() {
+    alert("Yay, it works!");
+}
